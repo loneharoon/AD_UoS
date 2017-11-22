efa216b3ff478d2d934f6ba3c63ce007eb892789
Haroon Rashid <haroonr@iiitd.ac.in>
2017-11-22 15:40:35 +0000

    final code for single home

--- plot_REFIT.py
+++ plot_REFIT.py
@@ -9,8 +9,10 @@ Created on Tue Nov 21 14:35:12 2017
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+sns.set(style="ticks",color_codes=True)
 import numpy as np
-
+from matplotlib.backends.backend_pdf import PdfPages
+import inspect
 #%%
 dir = "/Volumes/MacintoshHD2/Users/haroonr/Detailed_datasets/REFITT/dataset_10mins/"
 home = "House1.csv"
@@ -19,10 +21,9 @@ df = pd.read_csv(dir+home,index_col="localminute")
 #%%
 # this plots data of each appliance separtely in row fashion manner
 df = df/1000
-df.plot(subplots=True,figsize=(12,10))
-#plt.savefig(savedir+"house1.pdf",bbox_inches="tight")
-plt.savefig(savedir+"house1.pdf")
-plt.close()
+#df.plot(subplots=True,figsize=(12,10))
+#plt.savefig(savedir+"house1.pdf")
+#plt.close()
 #%%
 df.index = pd.to_datetime(df.index)
 temp = df['Aggregate']
@@ -31,14 +32,25 @@ temp = temp.to_frame()
 temp['day']= temp.index.date
 temp['timestamp'] = temp.index.time
 #%%
-sns.set(style="ticks",color_codes=True)
-h =  sns.FacetGrid(temp,col='day',col_wrap=7,size=2.5)
-(h.map_dataframe(plt.plot,'timestamp','Aggregate')
-.set_axis_labels("Timestamp","Power(kW)")
-.fig.subplots_adjust(wspace=.2,hspace=.5))
+#sns.set(style="ticks",color_codes=True)
+#h =  sns.FacetGrid(temp,col='day',col_wrap=7,size=2.5)
+#(h.map_dataframe(plt.plot,'timestamp','Aggregate')
+#       .set_axis_labels("Timestamp","Power(kW)")
+#       .fig.subplots_adjust(wspace=.2,hspace=.5))
 #h.savefig("/Volumes/MacintoshHD2/Users/haroonr/Downloads/haroon_temp.pdf")
 #%%
-temp2 = df['Aggregate']['2013']
+temp2 = df['Aggregate']
 temp2 = temp2.to_frame()
-temp2['date'] = temp2.index.date
-grouped_1 = temp2.groupby(temp2.index.month)
+temp2['day'] = temp2.index.date
+temp2['timestamp'] = temp2.index.hour * 60 + temp2.index.minute
+# VERIFY COLUMN NAMES
+temp2  =  temp2.rename(columns={'Aggregate' : 'power'})
+grouped_1 = temp2.groupby([temp2.index.year,temp2.index.month])
+#%%
+with PdfPages('demo_haroon.pdf') as pdf:
+  for i,group in grouped_1:
+    plt.figure()
+    print(group)
+    ob = plot_facet_plots(group)
+    pdf.savefig(ob)
+    
