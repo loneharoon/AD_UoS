fd68e6136e3198afb2e80772af91a68e15671ed6
Haroon Rashid <haroonr@iiitd.ac.in>
2017-12-18 14:56:24 +0000

    anomaly stats

---
+++ anomaly_stat_REFIT.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This file performs different stats on the anomalies collected from REFTI dataset
+Created on Mon Dec 18 11:40:59 2017
+
+@author: haroonr
+"""
+#%%
+import numpy as np
+import pandas as pd
+from copy import deepcopy
+from collections import Counter
+#%%
+loc = "/Volumes/MacintoshHD2/users/haroonr/Dropbox/UniOfStra/AD/"
+fl = "anomaly_explanation.csv"
+df = pd.read_csv(loc+fl)
+#%%
+# mapping for different appliance present in the sheet
+re_map = {'ChestFreezer':'Freezer','Freezer_1':'Freezer','Freezer_2':'Freezer',
+          'Freezer_garage': 'Freezer','Freezer_1':'Freezer','Fridge-Freezer_1':'Fridge-Freezer'\
+          ,'Fridge-Freezer_2':'Fridge-Freezer','FridgeFreezer':'Fridge-Freezer','Firdge':'Fridge'\
+          ,'Fridge_garag':'Fridge'}
+
+
+def remap_appliances_ifrequired(df,re_map):
+  ''' this renames appliance if it is named with some other name'''
+  frame = df.copy()
+  updated = []
+  for i in frame.Appliance:
+    if i in re_map:
+      updated.append(re_map[i])
+    else:
+      updated.append(i)
+  frame.Appliance = updated
+  return(frame)
+  
+dfx = remap_appliances_ifrequired(df,re_map)
+Counter(dfx.Appliance)
+
+ 
