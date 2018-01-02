1a221de3ecb7ed8c8bac62555fa527e4520ef9ff
Haroon Rashid <haroonr@iiitd.ac.in>
2018-01-02 21:27:05 +0000

    updated

--- AD_main.py
+++ AD_main.py
@@ -9,6 +9,8 @@ Created on Tue Jan  2 08:53:47 2018
 #%%
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import numpy as np
 #%%
 dir = "/Volumes/MacintoshHD2/Users/haroonr/Detailed_datasets/REFITT/CLEAN_REFIT_081116/"
 home = "House1.csv"
@@ -16,4 +18,16 @@ df = pd.read_csv(dir+home,index_col="Time")
 df.index = pd.to_datetime(df.index)
 df_sub = df["2014-03":] # since before march their are calibration issues
 #%% Resampling data
+print("*****RESAMPLING********)
 df_samp = df_sub.resample('1T',label='right',closed='right').mean()
+#%% select particular appliance for anomaly detection
+df_samp.columns
+myapp = "Freezer_1"
+app_data = df_samp[myapp]
+print(app_data.head(1))
+print(app_data.tail(1))
+#%%perform clustering
+samp =  app_data['2014-03-07'].values
+samp = samp.reshape(-1,1)
+perform_clustering(samp)
+#%% obtain cycle stats
--- AD_support.py
+++ AD_support.py
@@ -1,8 +1,58 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+This contains different function defintions for anomaly detection on REFIT
 Created on Tue Jan  2 08:54:04 2018
 
 @author: haroonr
 """
 
+def perform_clustering(samp):
+  #TODO: this has not been completed yet
+  # http://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html#sklearn.cluster.KMeans
+  kmeans = KMeans(n_clusters=2, random_state=0).fit(samp)
+  #kmeans.labels_
+  #kmeans.cluster_centers_
+  return (kmeans)
+
+
+
+def find_cycle_parameters(app_daywise,sampling_time):
+  #sampling_time = 2*60 # 2 minutes
+  cycle_stat = {}
+  cycles = [] # no  of cycles
+  duration = [] # no. of cycles duration
+  energy = [] # no of cycles energy
+      if data[i] > 10 and data[i-1] < 2:
+        count = count + 1
+        ontime = ontime + 1
+        energycount =  energycount + data[i]
+  for day in app_daywise:
+    data = day[1] # day[1] is item and day[0] is key
+    count = 0
+    ontime = 0
+    energycount = 0
+    for i in range(1,data.shape[0]): 
+      elif data[i] > 10 and data[i-1] > 10: # in same ON state
+        ontime = ontime + 1
+        energycount = energycount + data[i]
+    cycles.append(count)
+    duration.append(ontime)
+    energy.append(energycount)
+    
+  cycles_unique = np.unique(cycles)
+  df = pd.DataFrame({'cycles':cycles,'duration':duration,'energy':energy})
+  appliance_duration = df.groupby(cycles)['duration'].mean() * sampling_time
+  energy_appliance =  df.groupby(cycles)['energy'].mean()
+  samples = Counter(cycles) 
+  # calculate cycle probabilites
+  samples_ordered = OrderedDict(sorted(samples.items()))
+  frequency = list(samples_ordered.values())
+  frequency_sum = np.sum(frequency)
+  cyclesprob = [i/frequency_sum for i in frequency]
+  cycle_stat['numberOfCycles'] = cycles_unique.tolist()
+  cycle_stat['numberOfSamples'] = list(dict(samples).values())
+  cycle_stat['numberOfCyclesProb'] = cyclesprob
+  cycle_stat['numberOfCyclesEnergy'] = energy_appliance.tolist()
+  cycle_stat['numberOfCyclesDuration'] = appliance_duration.tolist()
+  return(cycle_stat)
