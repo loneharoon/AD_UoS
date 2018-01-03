5303f73f43e6c73bf1917439adcf5c7ffe050f07
Haroon Rashid <haroonr@iiitd.ac.in>
2018-01-03 11:48:26 +0000

    code updated

--- AD_main.py
+++ AD_main.py
@@ -11,6 +11,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import numpy as np
+from itertools import groupby
 #%%
 dir = "/Volumes/MacintoshHD2/Users/haroonr/Detailed_datasets/REFITT/CLEAN_REFIT_081116/"
 home = "House1.csv"
@@ -18,8 +19,9 @@ df = pd.read_csv(dir+home,index_col="Time")
 df.index = pd.to_datetime(df.index)
 df_sub = df["2014-03":] # since before march their are calibration issues
 #%% Resampling data
-print("*****RESAMPLING********)
+print("*****RESAMPLING********")
 df_samp = df_sub.resample('1T',label='right',closed='right').mean()
+data_sampling_time = 1 #in minutes
 #%% select particular appliance for anomaly detection
 df_samp.columns
 myapp = "Freezer_1"
@@ -27,7 +29,14 @@ app_data = df_samp[myapp]
 print(app_data.head(1))
 print(app_data.tail(1))
 #%%perform clustering
-samp =  app_data['2014-03-07'].values
-samp = samp.reshape(-1,1)
-perform_clustering(samp)
+samp = app_data['2014-03-07'].to_frame()
+samp_val =  samp.values
+samp_val = samp_val.reshape(-1,1)
+kobj = perform_clustering(samp_val,clusters=2)
+#FIXME:
+samp['cluster'] = kobj.labels_
 #%% obtain cycle stats
+
+temp1 = [(k,sum(1 for i in g)) for k,g in groupby(kobj.labels_)]
+temp1 = pd.DataFrame(temp1,columns=['cluster','cluster_length'])
+temp1['duration'] = temp1['cluster_length'] * data_sampling_time
