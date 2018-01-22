b8848f9abba91f99841a5326992dd8cae39acd9a
Haroon Rashid <haroonr@iiitd.ac.in>
2018-01-22 14:41:20 +0000

    added energy comp. parameter

--- AD_main.py
+++ AD_main.py
@@ -26,6 +26,7 @@ df_sub = df["2014-03":] # since before march their are calibration issues
 print("*****RESAMPLING********")
 df_samp = df_sub.resample('1T',label='right',closed='right').mean()
 data_sampling_time = 1 #in minutes
+data_sampling_type = "minutes" # or seconds
 #%% select particular appliance for anomaly detection
 df_samp.columns
 myapp = "Freezer_1"
@@ -39,27 +40,31 @@ print(app_data.tail(1))
      4. calculate above stats """
 myapp = "Freezer_1"
 # set training data duration
-train_data =  df_samp[myapp]['2014-03']
+train_data =  df_samp[myapp]['2014-03-07']
 # divide data according to  4 contexts [defined by times]
 contexts = OrderedDict()
 contexts['night_1_gp'] = train_data.between_time("00:00","05:59")
 contexts['day_1_gp'] = train_data.between_time("06:00","11:59")
 contexts['day_2_gp'] = train_data.between_time("12:00","17:59")
 contexts['night_2_gp'] = train_data.between_time("18:00","23:59")
-#%%
+#%
 # create groups within contexts day wise, this will allow us to catch stats at day level otherwise preserving boundaries between different days might become difficult
-contexts_daywise = {}
+contexts_daywise = OrderedDict()
 for k,v in contexts.items():
   contexts_daywise[k] = v.groupby(v.index.date)
- #%% Compute stats context wise
+ #% Compute stats context wise
 contexts_stats = OrderedDict()
+#%%
+print
 for k,v in contexts_daywise.items():
-  contexts_stats[k] = create_training_stats(v)
-  print("training stats of context {} is done".format(k))
+  print("CONTEXT IS {}".format(k))
+  contexts_stats[k] = create_training_stats(v,sampling_type=data_sampling_type,sampling_rate=data_sampling_time) # minutes or seconds
+  
+  #print("training stats of context {} is done".format(k))
 #%% TESTING STAGE STARTS
 #prepare test data
-test_data =  df_samp[myapp]['2014-04-08':'2014-04-08']
-#test_data =  df_samp[myapp]['2014-04-01']
+test_data =  df_samp[myapp]['2014-04-08':'2014-08-30']
+#test_data =  df_samp[myapp]['2014-08-01']
 test_data_daywise = test_data.groupby(test_data.index.date) # daywise grouping
 test_contexts_daywise = OrderedDict()
 for k,v in test_data_daywise:     # context wise division
@@ -70,69 +75,24 @@ for k,v in test_data_daywise:     # context wise division
   test_contexts['day_2_gp']   = v.between_time("12:00","17:59")
   test_contexts['night_2_gp'] = v.between_time("18:00","23:59")
   test_contexts_daywise[str(k)] = test_contexts
-#%%
+#%
 test_stats = OrderedDict()
 for day,data in test_contexts_daywise.items():
   print("testing for day {}".format(day))
-  temp = {}
+  temp = OrderedDict()
   for context,con_data in data.items():
     #temp[context] = create_testing_stats(con_data,context)
-    temp[context] = create_testing_stats_with_boxplot(con_data,context)
+    res = create_testing_stats_with_boxplot(con_data,context)
+    if res!= False:
+      temp[context] = res
+    else:
+      continue   
   test_stats[day] = temp
-#%% AD logic starts now
-# test_stats - contains stats computed on test day
-#contexts_stats - contains stats computed from training data   
-          
-import logging
-import logging.handlers
-LOG_FILENAME = '/Volumes/MacintoshHD2/Users/haroonr/Downloads/REFIT_log/logfile_REFIT.out'
-my_logger = logging.getLogger('MyLogger')
-my_logger.setLevel(logging.INFO)
-# Add the log message handler to the logger
-handler = logging.handlers.RotatingFileHandler(
-              LOG_FILENAME, maxBytes=9000000, backupCount=0)
-my_logger.addHandler(handler)
 
-result = []
-num_std = 2.5
-dummy_no = 2
-for day,data in test_stats.items():
-  for contxt,contxt_stats in data.items():
-    #be clear - word contexts_stats represents training data and word contxt represents test day stats
-    train_results = contexts_stats[contxt] # all relevant train stats
-    test_results  = contxt_stats
-    temp_res = {}
-    temp_res['timestamp'] = datetime.strptime(day,'%Y-%m-%d')
-    temp_res['context'] = contxt
-    temp_res['status'] = 0
-    temp_res['anomtype'] = ' '
-    if (test_results['ON_duration']['mean'] >  train_results['ON_duration']['mean'] + num_std* train_results['ON_duration']['std']) and (test_results['OFF_duration']['mean'] >  train_results['OFF_duration']['mean'] + num_std* train_results['OFF_duration']['std']):
-       temp_res['status'] = 0
-       my_logger.info(day + ":" + contxt + "is not elongated anomaly as off time was also longer")
-    elif test_results['ON_duration']['mean'] > dummy_no * train_results['ON_duration']['mean'] + num_std* train_results['ON_duration']['std']:
-       temp_res['status'] = 1
-       temp_res['anomtype'] = "long"
-       my_logger.info(day + ":"+ contxt + ", elongated anomaly" + ", train_stats duration, " + str(train_results['ON_duration']['mean']) + ":"+str(train_results['ON_duration']['std']) + "; test_stats duration, " + str(test_results['ON_duration']['mean']) )
-    elif test_results['ON_cycles']['mean'] >  dummy_no * train_results['ON_cycles']['mean'] + num_std* train_results['ON_cycles']['std']:
-       temp_res['status'] = 1
-       temp_res['anomtype'] = "frequent"
-       my_logger.info(day + ":"+contxt +  ", frequent anomaly" + ", train_stats frequency, " + str(train_results['ON_cycles']['mean']) + ":"+str(train_results['ON_cycles']['std']) + "; test_stats frequency, " + str(test_results['ON_cycles']['mean']) )
-    result.append(temp_res)
-res_df = pd.DataFrame.from_dict(result)
-res_df = res_df.sort_values('timestamp')
-res_df[res_df.status==1]
-res_df[res_df.status==1].shape[0]
       
-#%%
-import logging
-import logging.handlers
-LOG_FILENAME = '/Volumes/MacintoshHD2/Users/haroonr/Downloads/REFIT_log/logfile_REFIT_2.txt'
-#mylogger = logging.getLogger('MyLogger')
-#mylogger.setLevel(logging.INFO)
-## Add the log message handler to the logger
-#handler = logging.handlers.RotatingFileHandler(
-#              LOG_FILENAME, maxBytes=9000000, backupCount=0)
-#mylogger.addHandler(handler)
+#%% Anomaly detection logic
+
+LOG_FILENAME = '/Volumes/MacintoshHD2/Users/haroonr/Downloads/REFIT_log/logfile_REFIT.csv'
 with open(LOG_FILENAME,'a') as mylogger:
 
   mylogger.write("\n*****NEW ITERATION at time {}*************\n".format(datetime.now()))
@@ -158,39 +118,43 @@ with open(LOG_FILENAME,'a') as mylogger:
       
       lower_ON_cycles  =   test_results['ON_cycles'] < train_results['ON_cycles']['lowerwisker'] 
       higher_ON_cycles =   test_results['ON_cycles'] > train_results['ON_cycles']['upperwisker']  
-      ON_cycles_outlier = lower_ON_cycles + higher_ON_cycles
+      #ON_cycles_outlier = lower_ON_cycles + higher_ON_cycles
+      #lower ON cycles will include OFF cases
+      ON_cycles_outlier =  higher_ON_cycles
       
       lower_OFF_cycles  =   test_results['OFF_cycles'] < train_results['OFF_cycles']['lowerwisker']  
       higher_OFF_cycles =   test_results['OFF_cycles'] > train_results['OFF_cycles']['upperwisker'] 
       OFF_cycles_outlier = lower_OFF_cycles + higher_OFF_cycles
           #logging
       if lower_ON_outlier:
-        mylogger.write(day+":"+" lower_ON_outlier_count: "+ str(lower_ON_outlier)+ " lower_wisker: "+ str(train_results['ON_duration']['lowerwisker']) + ", test_Case : " + str(test_results['ON_duration'])+"\n")
+        mylogger.write(day+":"+contxt+":"+" lower_ON_outlier_count: "+ str(lower_ON_outlier)+ " lower_wisker: "+ str(train_results['ON_duration']['lowerwisker']) + ", test_Case : " + str(test_results['ON_duration'])+"\n")
       if higher_ON_outlier:
-        mylogger.write(day+":"+" higher_ON_outlier_count: "+ str(higher_ON_outlier)+ " upper_wisker: "+ str(train_results['ON_duration']['upperwisker']) + ", test_Case : " + str(test_results['ON_duration'])+"\n")
+        mylogger.write(day+":"+contxt+":"+" higher_ON_outlier_count: "+ str(higher_ON_outlier)+ " upper_wisker: "+ str(train_results['ON_duration']['upperwisker']) + ", test_Case : " + str(test_results['ON_duration'])+"\n")
       if lower_OFF_outlier:
-        mylogger.write(day+":"+" lower_OFF_outlier_count: "+ str(lower_OFF_outlier)+ " lower_wisker: "+ str(train_results['OFF_duration']['lowerwisker']) + ", test_Case : " + str(test_results['OFF_duration'])+"\n")
+        mylogger.write(day+":"+contxt+":"+" lower_OFF_outlier_count: "+ str(lower_OFF_outlier)+ " lower_wisker: "+ str(train_results['OFF_duration']['lowerwisker']) + ", test_Case : " + str(test_results['OFF_duration'])+"\n")
       if higher_OFF_outlier:
-        mylogger.write(day+":"+" higher_OFF_outlier_count: "+ str(higher_OFF_outlier)+ " upper_wisker: "+ str(train_results['OFF_duration']['upperwisker']) + ", test_Case : " + str(test_results['OFF_duration'])+"\n")
+        mylogger.write(day+":"+contxt+":"+" higher_OFF_outlier_count: "+ str(higher_OFF_outlier)+ " upper_wisker: "+ str(train_results['OFF_duration']['upperwisker']) + ", test_Case : " + str(test_results['OFF_duration'])+"\n")
   
-      if lower_ON_cycles:
-        mylogger.write(day+":"+" lower_ON_cycles_count: "+ str(lower_ON_cycles)+ " lower_wisker: "+ str(train_results['ON_cycles']['lowerwisker']) + ", test_Case : " + str(test_results['ON_cycles'])+"\n")
+#      if lower_ON_cycles:
+#        mylogger.write(day+":"+contxt+":"+" lower_ON_cycles_count: "+ str(lower_ON_cycles)+ " lower_wisker: "+ str(train_results['ON_cycles']['lowerwisker']) + ", test_Case : " + str(test_results['ON_cycles'])+"\n")
       if higher_ON_cycles:
-        mylogger.write(day+":"+" higher_ON_cyclesr_count: "+ str(higher_ON_cycles)+ " upper_wisker: "+ str(train_results['ON_cycles']['upperwisker']) + ", test_Case : " + str(test_results['ON_cycles'])+"\n")
-      if lower_OFF_cycles:
-        mylogger.write(day+":"+" lower_OFF_cycles_count: "+ str(lower_OFF_cycles)+ " lower_wisker: "+ str(train_results['OFF_cycles']['lowerwisker']) + ", test_Case : " + str(test_results['OFF_cycles'])+"\n")
-      if higher_OFF_cycles:
-        mylogger.write(day+":"+" higher_OFF_cycles_count: "+ str(higher_OFF_cycles)+ " upper_wisker: "+ str(train_results['OFF_cycles']['upperwisker']) + ", test_Case : " + str(test_results['OFF_cycles'])+"\n")  
+        mylogger.write(day+":"+contxt+":"+" higher_ON_cyclesr_count: "+ str(higher_ON_cycles)+ " upper_wisker: "+ str(train_results['ON_cycles']['upperwisker']) + ", test_Case : " + str(test_results['ON_cycles'])+"\n")
+#      if lower_OFF_cycles:
+#        mylogger.write(day+":"+contxt+":"+" lower_OFF_cycles_count: "+ str(lower_OFF_cycles)+ " lower_wisker: "+ str(train_results['OFF_cycles']['lowerwisker']) + ", test_Case : " + str(test_results['OFF_cycles'])+"\n")
+#      if higher_OFF_cycles:
+#        mylogger.write(day+":"+contxt+":"+" higher_OFF_cycles_count: "+ str(higher_OFF_cycles)+ " upper_wisker: "+ str(train_results['OFF_cycles']['upperwisker']) + ", test_Case : " + str(test_results['OFF_cycles'])+"\n")  
       
       if ON_duration_outlier and OFF_duration_outlier:
         temp_res['status'] = 0
-        print ("non anomalous")
+        print ("non anomalous on {} with context {}".format(day,contxt))
       elif ON_duration_outlier:
         temp_res['status'] = 1
         temp_res['anomtype'] = 'elongated'
+        print ("Elongated anomaloy on {} with context {}".format(day,contxt))
       elif ON_cycles_outlier:
         temp_res['status'] = 1
         temp_res['anomtype'] = 'frequent'
+        print ("Frequent anomaloy on {} with context {}".format(day,contxt))
       result.append(temp_res)
 res_df = pd.DataFrame.from_dict(result)
 res_df = res_df.sort_values('timestamp')
--- AD_support.py
+++ AD_support.py
@@ -21,7 +21,7 @@ def perform_clustering(samp,clusters):
   #kmeans.labels_
   #kmeans.cluster_centers_
   return (kmeans)
-
+###
 def re_organize_clusterlabels(samp):
   """this function checks if labels assigned to data are correct. Less consumption should get lower label and higher should get high label. Doing This maintains consistency across different days and datasets and allows comparison
  input: samp pandas dataframe has  columns: power and cluster
@@ -41,9 +41,10 @@ def re_organize_clusterlabels(samp):
      samp.drop('new_cluster',axis=1,inplace=True)
   return (samp)
 #%%
+###
 def compute_boxplot_stats(boxdata):
   ''' Here i compute all stats of boxplot and return them as dictionary'''
-  boxdict = {}
+  boxdict = OrderedDict()
   nmedian =  np.median(boxdata)
   istquat =  np.percentile(boxdata,0)
   thirdquat =  np.percentile(boxdata,100)
@@ -53,11 +54,12 @@ def compute_boxplot_stats(boxdata):
   boxdict['upperwisker'] =  nmedian + 1.5 * iqr
   return (boxdict)
 #%%
-def create_training_stats(traindata):
+###
+def create_training_stats(traindata,sampling_type,sampling_rate):
   """ this method computes cycle frequences and durations from the training data
   Input: pandas series of power data in the python groupby object
   Output: Stats computed in form of dictionary """
-  dic = {}
+  dic = OrderedDict()
   for k, v in traindata:
     #print(k)
     samp = v.to_frame()
@@ -80,39 +82,68 @@ def create_training_stats(traindata):
     samp = re_organize_clusterlabels(samp)
     tempval = [(k,sum(1 for i in g)) for k,g in groupby(samp.cluster.values)]
     tempval = pd.DataFrame(tempval,columns=['cluster','samples'])
+    #%energy computation logic for eacy cycle
+    samp['state_no']  = np.repeat(range(tempval.shape[0]),tempval['samples'])
+    samp_groups = samp.groupby(samp.state_no)
+    energy_state= [np.sum(v.power) for k,v in samp_groups]
+    if sampling_type =='minutes':
+      energy_state = np.multiply(energy_state, (sampling_rate/60.))
+    elif sampling_type == 'seconds':
+      energy_state = np.multiply(energy_state, (sampling_rate/3600.)) 
+    tempval['energy_state'] =  np.round(energy_state,2)
+   #% energy logic ends
     off_cycles =list(tempval[tempval.cluster==0].samples)
     on_cycles =list(tempval[tempval.cluster==1].samples)
+    off_energy =list(tempval[tempval.cluster==0].energy_state)
+    on_energy =list(tempval[tempval.cluster==1].energy_state)
     temp_dic = {}
     temp_dic["on"] = on_cycles
     temp_dic["off"] = off_cycles
+    temp_dic["on_energy"] = on_energy
+    temp_dic["off_energy"] = off_energy
     cycle_stat = Counter(tempval.cluster)
     temp_dic.update(cycle_stat)
     dic[str(k)] = temp_dic
     #% Merge  OFF and ON states of different days into singe lists 
   ON_duration = []
   OFF_duration = []
+  ON_energy = []
+  OFF_energy = []
   ON_cycles = []
   OFF_cycles = []
   for k,v in dic.items():
     ON_duration.append(v['on'])
     OFF_duration.append(v['off'])
+    ON_energy.append(v['on_energy'])
+    OFF_energy.append(v['off_energy'])
     ON_cycles.append(v[1])
     OFF_cycles.append(v[0])
   ON_duration  =  [ item for sublist in ON_duration for item in sublist]
   OFF_duration = [ item for sublist in OFF_duration for item in sublist]
- #%
+  ON_energy  =  [ item for sublist in ON_energy for item in sublist]
+  OFF_energy = [ item for sublist in OFF_energy for item in sublist]
+   #%
   summ_dic = {}
   #for boxplot logic  
   summ_dic['ON_duration'] = {'mean':round(np.mean(ON_duration),3), 'std':round(np.std(ON_duration),3)}
   summ_dic['ON_duration'].update(compute_boxplot_stats(ON_duration))
   summ_dic['OFF_duration'] = {'mean':round(np.mean(OFF_duration),3), 'std':round(np.std(OFF_duration),3)}
   summ_dic['OFF_duration'].update(compute_boxplot_stats(OFF_duration))
+  summ_dic['ON_energy'] = {'mean':round(np.mean(ON_energy),3), 'std':round(np.std(ON_energy),3)}
+  summ_dic['ON_energy'].update(compute_boxplot_stats(ON_energy))
+  summ_dic['OFF_energy'] = {'mean':round(np.mean(OFF_energy),3), 'std':round(np.std(OFF_energy),3)}
+  summ_dic['OFF_energy'].update(compute_boxplot_stats(OFF_energy))
   summ_dic['ON_cycles'] = {'mean':round(np.mean(ON_cycles),0), 'std':round(np.std(ON_cycles),3)}
   summ_dic['ON_cycles'].update(compute_boxplot_stats(ON_cycles))
   summ_dic['OFF_cycles'] = {'mean':round(np.mean(OFF_cycles),0), 'std':round(np.std(OFF_cycles),3)}
   summ_dic['OFF_cycles'].update(compute_boxplot_stats(OFF_cycles))
+  #for debugging purpose
+  print(compute_boxplot_stats(ON_duration))
+  print(compute_boxplot_stats(OFF_duration))
+  print(compute_boxplot_stats(ON_cycles))
   return (summ_dic)
 #%%
+###
 def create_testing_stats(testdata,k):
   """ this method computes cycle frequences and durations for the test day data
   Input: pandas series of power data
@@ -127,7 +158,7 @@ def create_testing_stats(testdata,k):
   if nan_obs:  
     if nan_obs >= 0.50*samp.shape[0]:
       print("More than 50percent obs missing hence dropping context {} ".format(k))
-      #continue
+      return
     elif nan_obs < 0.50*samp.shape[0]:
       print("dropping  {} nan observations for context {}".format(nan_obs,k))
       samp.dropna(inplace=True)
@@ -178,14 +209,17 @@ def create_testing_stats_with_boxplot(testdata,k):
   if nan_obs:  
     if nan_obs >= 0.50*samp.shape[0]:
       print("More than 50percent obs missing hence dropping context {} ".format(k))
-      #continue
+      return (False)
     elif nan_obs < 0.50*samp.shape[0]:
-      print("dropping  {} nan observations for context {}".format(nan_obs,k))
+      print("dropping  {} nan observations for total of {} in context {}".format(nan_obs, samp.shape[0], k))
       samp.dropna(inplace=True)
   samp.columns = ['power']
   samp_val =  samp.values
   samp_val = samp_val.reshape(-1,1)
   #FIXME: you can play with clustering options
+  if np.std(samp_val) <= 1:# contains observations with same values, basically forward filled values
+    print("Dropping context {} from analysis as it contains same readings".format(k))
+    return (False)
   kobj = perform_clustering(samp_val,clusters=2)
   samp['cluster'] = kobj.labels_
   samp = re_organize_clusterlabels(samp)
@@ -202,7 +236,7 @@ def create_testing_stats_with_boxplot(testdata,k):
 #  summ_dic['OFF_duration'] = {'mean':round(np.mean(temp_dic["off"]),3), 'std':round(np.std(temp_dic["off"]),3)}
 #  summ_dic['ON_cycles'] = {'mean':round(np.mean(temp_dic[1]),0), 'std':round(np.std(temp_dic[1]),3)}
 #  summ_dic['OFF_cycles'] = {'mean':round(np.mean(temp_dic[0]),0), 'std':round(np.std(temp_dic[0]),3)}
-  summ_dic = {}
+  summ_dic = OrderedDict()
   summ_dic['ON_duration'] = temp_dic["on"]
   summ_dic['OFF_duration'] = temp_dic["off"]
   summ_dic['ON_cycles'] = temp_dic[1]
