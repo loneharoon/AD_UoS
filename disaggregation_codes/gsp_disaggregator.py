6c0c955ef0d490239bd44ae415d6c86f7753d4f2
Haroon Rashid <haroonr@iiitd.ac.in>
2018-02-01 22:22:05 +0000

    adding another disagg

---
+++ disaggregation_codes/gsp_disaggregator.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+In this I implement Bochao's GSP disaggregation method
+Created on Thu Feb  1 15:42:41 2018
+
+@author: haroonr
+"""
+import warnings
+warnings.filterwarnings("ignore")
+import numpy as np
+import pandas as pd
+import sys
+sys.path.append('/Volumes/MacintoshHD2/Users/haroonr/Dropbox/UniOfStra/AD/disaggregation_codes/')
+import accuracy_metrics_disagg as acmat
+import matplotlib.pyplot as plt
+#%%
+dir = "/Volumes/MacintoshHD2/Users/haroonr/Detailed_datasets/REFITT/CLEAN_REFIT_081116/"
+home = "House1.csv"
+df = pd.read_csv(dir+home,index_col="Time")
+df.index = pd.to_datetime(df.index)
+df_sub = df["2014-03-01":'2014-04-30'] # since before march their are calibration issues
+#%% Resampling data
+print("*****RESAMPLING********")
+df_samp = df_sub.resample('1T',label='right',closed='right').mean()
+data_sampling_time = 1 #in minutes
+data_sampling_type = "minutes" # or seconds
+df_samp.drop('Issues',axis=1,inplace=True)
+df_samp.rename(columns={'Aggregate':'use'},inplace=True) # renaming agg column
+#%%
+data_vec = df_samp['use'].values.tolist()
+delta_p = [round(data_vec[i+1]-data_vec[i],2) for i in range(0,len(data_vec)-1)]
+T = 80;
+Sigma = 15;
+Ri = 0.05;
+events = [True if delta_p[i] > T or delta_p[i] < -T else False for i in range(0, len(delta_p))]
+
+T_Positive = 40;
+T_Negative = -40;
