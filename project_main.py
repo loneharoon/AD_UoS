f1fbb04cf8f7fd1211d150309eecbf178ece1452
Haroon Rashid <haroonr@iiitd.ac.in>
2018-02-22 15:10:09 +0000

    corrected True False evaluation

--- project_main.py
+++ project_main.py
@@ -22,14 +22,14 @@ import latent_Bayesian_melding as LBM
 #%%
 #dir = "/Volumes/MacintoshHD2/Users/haroonr/Detailed_datasets/REFITT/CLEAN_REFIT_081116/"
 dir = "/Volumes/MacintoshHD2/Users/haroonr/Detailed_datasets/REFITT/REFIT_selected/"
-home = "House16.csv"
+home = "House10.csv"
 df = pd.read_csv(dir+home,index_col="Time")
 df.index = pd.to_datetime(df.index)
 #df_sub = df["2014-03-01":'2014-04-30']
 #df_sub = df["2014-04-01":] # since before march their are calibration issues
 df_sub = deepcopy(df[:])
 #%% Resampling data
-resample = 'True'
+resample = True
 data_sampling_time = 1 #in minutes
 data_sampling_type = "minutes" # or seconds
 if resample: 
@@ -44,43 +44,46 @@ else:
   df_samp = deepcopy(df_sub)
   df_samp.drop('Issues',axis=1,inplace=True)
   standardize_column_names.rename_appliances(home,df_samp) # this renames columns  
-#%%
+
 energy = df_samp.sum(axis=0)
 high_energy_apps = energy.nlargest(7).keys() # CONTROL : selects few appliances
 df_selected = df_samp[high_energy_apps]
-#%% 
-denoised = 'False'
+denoised = False
 if denoised:
     # chaning aggregate column
     iams = high_energy_apps.difference(['use'])
     df_selected['use'] = df_selected[iams].sum(axis=1)
     print('**********DENOISED DATA*************8')
 #%%
-#train_dset = df_selected['2014-04-01':'2014-04-30'] # home10
+train_dset = df_selected['2014-04-01':'2014-04-30'] # home10
 #train_dset = df_selected['2014-05-01':'2014-05-31'] # home20
 #train_dset = df_selected['2014-07-01':'2014-07-31'] # home18
-train_dset = df_selected['2014-03-01':'2014-03-31'] # home16
+#train_dset = df_selected['2014-03-01':'2014-03-31'] # home16
+#train_dset = df_selected['2014-12'] # home1
 train_dset.dropna(inplace=True)
 
-#test_dset = df_selected['2014-05-01':] #home 10
+test_dset = df_selected['2014-05-01':] #home 10
 #test_dset = df_selected['2014-06-01':] #home 20
 #test_dset = df_selected['2014-08-01':] #home 18
-test_dset = df_selected['2014-04-01':] #home 16
+#test_dset = df_selected['2014-04-01':] #home 16
+#test_dset = df_selected['2015-01':'2015-04'] #home 1
 test_dset.dropna(inplace=True)
 #%% RUN fHMM
 save_dir = "/Volumes/MacintoshHD2/Users/haroonr/Detailed_datasets/REFITT/Intermediary_results/"
+filename = save_dir+"noisy/fhmm/selected/"+ home.split('.')[0]+'.pkl'
 fhmm_result  =  localize_fhmm.fhmm_decoding(train_dset,test_dset) # dissagreation
+fhmm_result['actual_power']['use'] = test_dset['use']
 fhmm_result['train_power'] = train_dset
-filename = save_dir+"fhmm/selected/"+ home.split('.')[0]+'.pkl'
 handle = open(filename,'wb')
 #https://docs.python.org/2/library/pickle.html
 pickle.dump(fhmm_result,handle)
 handle.close()
 #%% RUN CO
 save_dir = "/Volumes/MacintoshHD2/Users/haroonr/Detailed_datasets/REFITT/Intermediary_results/"
+filename = save_dir+"noisy/co/selected/" + home.split('.')[0]+'.pkl'
 co_result = co.co_decoding(train_dset,test_dset)
+co_result['actual_power']['use'] = test_dset['use'] # appending aggregate column for later use
 co_result['train_power'] = train_dset
-filename = save_dir+"co/selected/" + home.split('.')[0]+'.pkl'
 handle = open(filename,'wb')
 pickle.dump(co_result,handle)
 handle.close()
