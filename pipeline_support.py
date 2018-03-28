c0be6247b7c777dd89230eb78613a4fb9b0ef512
Haroon Rashid <haroonr@iiitd.ac.in>
2018-03-28 10:24:20 +0100

    stable versions now

--- pipeline_support.py
+++ pipeline_support.py
@@ -189,18 +189,17 @@ def  interpolate_dataframe(start_ob, end_ob, temp_dup):
 #%%
 def smoothen_NILM_output(data_series, threshold_minutes, std, num_std):    
   '''this function taken NILM output and removes small OFF durations by using threshold_minutes''' 
-  #temp = test_data['2014-05-1'].to_frame()
-  #temp = test_data['2014-05-1'].to_frame()
-  threshold_minutes = threshold_minutes - std * num_std
+
+  threshold_minutes = threshold_minutes - num_std * std
   temp = data_series.to_frame()
-  # less than 5, I consider as OFF, this can be done with clustering too
-  status = [0 if i < 5 else 1 for i in temp.values]
+  # less than 10, I consider as OFF, this can be done with clustering too
+  status = [0 if i < 10 else 1 for i in temp.values]
   temp['status'] = status
   temp_groups = temp.groupby('status')
   tgt_gp = temp_groups.get_group(1)
   temp_dup = deepcopy(temp)
   temp_dup.columns = ['power','status']
-  temp_dup['power'].plot()
+  #temp_dup['power'].plot()
   #%
   for i in range(tgt_gp.shape[0]):
     j = i + 1
@@ -211,6 +210,7 @@ def smoothen_NILM_output(data_series, threshold_minutes, std, num_std):
     next_ind = tgt_gp.index[j]
     delta = next_ind - start_ind
     delta_minutes = (delta.seconds / 60)
+    #print (delta_minutes)
     if delta_minutes <= 1:
       pass
     elif delta_minutes < threshold_minutes:
@@ -218,8 +218,84 @@ def smoothen_NILM_output(data_series, threshold_minutes, std, num_std):
       temp_dup = interpolate_dataframe(start_ind, next_ind, temp_dup)
     else:
       pass
-  #temp_dup['power'].plot() 
+  # temp_dup['power'].plot() 
   return temp_dup['power']
+#%%
+def compute_AD_and_disagg_status_on_NILM_smoothened_data(logging_file,log_report, train_results, data_sampling_type,data_sampling_time, NoOfContexts,myapp,test_data,data_dic,disagg_approach,home,file_location,alpha,num_std):
+    ''' this is replica of compute_AD_and_disagg_status. It differs in few parameters. It is used in one of specific cases when NILM smoothened data is used '''
+    if log_report:
+      resultfile = open(logging_file,'a')
+      resultfile.write('*********************NEW HOME*****************\n')    
+    #train_results = ads.AD_refit_training(train_data,data_sampling_type,data_sampling_time, NoOfContexts,myapp)
+    test_results  = ads.AD_refit_testing(test_data,data_sampling_type,data_sampling_time, NoOfContexts,myapp)            
+    #%
+    if myapp == 'ElectricHeater':
+        res_df = ads.anomaly_detection_algorithm_ElectricHeater(test_results,train_results,alpha,num_std)
+    else:
+        res_df = ads.anomaly_detection_algorithm(test_results,train_results,alpha,num_std)
+    #result_sub = res_df[res_df.status==1]
+    #result_sub = res_df
+    # Compute disaggregation accuracies
+    norm_error = acmat.accuracy_metric_norm_error(data_dic)
+    order = format_results_in_appliance_order(home)
+    norm_error = norm_error.reindex(order)
+    # compute rmse too
+    rmse = acmat.compute_rmse_ver_dict(data_dic)
+    rmse = rmse.reindex(order)
+    cor_coeff = acmat.compute_correlation_ver_dict(data_dic)
+    cor_coeff = cor_coeff.reindex(order)
+    confus_mat = acmat.call_confusion_metrics_on_disagg(data_dic['actual_power'],data_dic['decoded_power'],power_threshold=10)
+    confus_mat = pd.DataFrame.from_dict(confus_mat)
+    confus_mat = confus_mat.reindex(order, axis = 1)
+    
+    if log_report:
+      resultfile.write('Following four disaggagregation metrics of {} approach \n'.format(disagg_approach))
+      resultfile.write("\n ANE is:\n")
+      resultfile.write(str(norm_error))
+      resultfile.write("\n RMSE is:\n")
+      resultfile.write(str(rmse))
+      resultfile.write("\n CORRELATION COEFFICIENT is:\n")
+      resultfile.write(str(cor_coeff))
+      resultfile.write("\n CONFUSION MAT is:\n")
+      resultfile.write(str(confus_mat))
+      resultfile.write('\n')
+      resultfile.close()
+    else:
+      print('Appliance normalization error are \n')
+      print(norm_error)
+      print('RMSE is:\n')
+      print(rmse)
+      print('correlation values are:\n')
+      print(cor_coeff) 
+      print('Confusion matrix accuracies are:\n')
+      print(confus_mat)
+    #%
+    # Compute anomaly detection accuracies
+    try:     
+        result_sub = res_df[res_df.anomtype == "long"]
+        print ("KEEPING LONG ANOMALIES, DROPPING FREQUENT ONES")
+    except:
+        result_sub = pd.DataFrame()
+        print("No long anomaly found in this case\n")
+    house_no =  int(re.findall('\d+',home)[0])
+    home = home.split('.')[0]+'.csv'
+    appliance = scn.reverse_lookup(home,myapp) # find actual name of appliance in anomaly database
+    assert len(appliance) > 1
+    day_start = test_data.first_valid_index()
+    day_end = test_data.last_valid_index()
+    gt, ob = ads.tidy_gt_and_ob(house_no, appliance, day_start, day_end, result_sub)
+    #confusion_matrix(gt.day.values,ob.day.values)
+    precision,recall, fscore = ads.compute_AD_confusion_metrics(gt, ob)
+    #print(precision,recall, fscore)  
+    if log_report:
+      resultfile = open(logging_file,'a')
+      resultfile.write("Anomaly detection accuracies at; context {}, alpha {}, std {} on {} data \n".format(NoOfContexts,alpha,num_std,disagg_approach))
+      resultfile.write('Precision, reall and f_score are: {}, {}, {} \n'.format(precision,recall, fscore))
+      resultfile.close()
+    else:
+      print("Anomaly detection accuracies at; context {}, alpha {}, std {} on {} data \n".format(NoOfContexts,alpha,num_std,disagg_approach))
+      print('Precision, reall and f_score are: {}, {}, {} \n'.format(precision,recall, fscore))
+  
     
     
     
--- understand_sshmm_tuning_for_AD.py
+++ understand_sshmm_tuning_for_AD.py
@@ -6,13 +6,14 @@ Created on Tue Mar 27 10:16:41 2018
 @author: haroonr
 """
 
-
+from __future__ import division
 import pickle
 import sys
 sys.path.append('/Volumes/MacintoshHD2/Users/haroonr/Dropbox/UniOfStra/AD/disaggregation_codes/')
 import AD_support as ads
 import pipeline_support as ps
 from copy import deepcopy
+
 #%% 
 file_location = "/Volumes/MacintoshHD2/Users/haroonr/Detailed_datasets/REFITT/Intermediary_results/"
 #TODO : TUNE ME
@@ -26,7 +27,7 @@ else:
   logging_file = " "
 
 #TODO : TUNE  US [we are 5]
-home = "House10.pkl" # options are: 10, 20, 18, 16, 1
+home = "House16.pkl" # options are: 10, 20, 18, 16, 1
 disagg_approach = "sshmms" # options are co,fhmm, lbm,sshmms,gsp
 
 NoOfContexts = 4
@@ -56,52 +57,25 @@ if disagg_approach == "lbm":
 train_data =  train_power[myapp]
 test_data =   decoded_power[myapp]
 actual_data = actual_power[myapp]
+#%% play with training data
+data_sampling_time = 1 #in minutes
+data_sampling_type = "minutes" # or s
+NoOfContexts, appliance = 4 ,'Freezer'
+train_results = ads.AD_refit_training(train_data,data_sampling_type,data_sampling_time,NoOfContexts,appliance)
 #%%
-temp = test_data['2014-05-11'].to_frame()
-status = [0 if i < 5 else 1 for i in temp.values]
-temp['status'] = status
-temp_groups = temp.groupby('status')
-tgt_gp = temp_groups.get_group(1)
-temp_dup = deepcopy(temp)
-temp_dup.columns = ['power','status']
-
+threshold_minutes = train_results['day_1_gp']['OFF_duration']['mean']
+std = train_results['day_1_gp']['OFF_duration']['std']
+# number of standard deviations
 #%%
-threshold_minutes = 10 # minutes
-for i in range(tgt_gp.shape[0]):
-  j = i + 1
-  if j >= tgt_gp.shape[0]:
-    print ('Loop limit reached\n')
-    break
-  start_ind = tgt_gp.index[i]
-  #while True:
-  next_ind = tgt_gp.index[j]
-  delta = next_ind - start_ind
-  delta_minutes = (delta.seconds / 60)
-  print(delta_minutes)
-  if delta_minutes <= 1:
-    pass
-  elif delta_minutes < threshold_minutes:
-    # interpolate me at minutes rate
-    print('interp')
-    temp_dup = interpolate_dataframe(start_ind, next_ind, temp_dup)
-  else:
-    pass
-      
-    
-      
+threshold_minutes = 10
+num_std = 0 # 0, 1, 1.5, 2, .....
+#data_series = test_data['2014-06-1']
+data_series = test_data['2014-06-1 04:00:00':'2014-06-1 05:00:00']
+#data_series = test_data
+nilm_smoothened = ps.smoothen_NILM_output(data_series, threshold_minutes, std, num_std)
+data_series.plot()
+nilm_smoothened.plot()
 #%%
-def  interpolate_dataframe(start_ob, end_ob, temp_dup):
-       ind = pd.date_range(start = start_ob, end = end_ob, freq = 'T')
-       temp_df =  pd.DataFrame(index = ind)
-       k = pd.concat([temp_dup.loc[str(start_ob)], temp_dup.loc[str(end_ob)]],axis = 1).T
-       k = k.combine_first(temp_df)
-       k['power'].interpolate(method = 'linear',inplace = True)
-       temp_dup.loc[k.index.intersection(temp_dup.index)] = k
-       return temp_dup
-       
-      
-    
-#%%
-
+ps.compute_AD_and_disagg_status_on_NILM_smoothened_data(logging_file,log_report,train_results, data_sampling_type,data_sampling_time, NoOfContexts,myapp, nilm_smoothened, data_dic, disagg_approach, home, file_location, alpha, num_std)
       
       
