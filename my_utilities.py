f429aba4da1730b9d57c0a31c478a0400ba29b56
Haroon Rashid <haroonr@iiitd.ac.in>
2018-04-03 08:36:52 +0100

    removed bug in merging

--- my_utilities.py
+++ my_utilities.py
@@ -10,9 +10,23 @@ Created on Tue Mar 20 11:21:34 2018
 #%%
 def create_pdf_from_pdf_list(file_list, savefilename):
     ''' combine pdfs '''
-    from PyPDF2 import PdfFileMerger
+    from PyPDF2 import PdfFileMerger, PdfFileReader
     merger = PdfFileMerger()
     for pdf in  file_list:
-        merger.append(open(pdf, 'rb'))
+        fin = PdfFileReader(open(pdf, 'rb')) 
+        merger.append(fin)
+    merger.write(savefilename)
+    
+        
+        
+
+def create_pdf_from_pdf_list_ver2(file_list, savefilename):
+    ''' Alternative of create_pdf_from_pdf_list, it uses different approach as, but results are same  '''
+    from PyPDF2 import PdfFileReader, PdfFileWriter
+    #merger = PdfFileMerger()
+    initial_output = PdfFileWriter()
+    for pdf in  file_list:
+        infile = PdfFileReader(open(pdf, "rb"))
+        initial_output.addPage(infile.getPage(0))
     with open(savefilename, 'wb') as fout:
-        merger.write(fout)
+        initial_output.write(fout)
