3332b9bc90d8b2f1f7d6128287b9ec5d514afa31
Haroon Rashid <haroonr@iiitd.ac.in>
2018-05-16 11:20:02 +0530

    added support for greek x legend

--- R_codes/plot_anomalous_&_nonanom_days.R
+++ R_codes/plot_anomalous_&_nonanom_days.R
@@ -5,22 +5,26 @@ library(data.table)
 library(fasttime)
 library(xts)
 Sys.setenv('TZ'='UTC')
-setwd('/Volumes/MacintoshHD2/Users/haroonr/Dropbox/Writings/UoS/plots/')
+setwd('/Volumes/MacintoshHD2/Users/haroonr/Dropbox/Writings/UoS/anom_plots/')
+
 rootdir <- "/Volumes/MacintoshHD2/Users/haroonr/Detailed_datasets/REFITT/REFIT_selected/"
-home <- "House10.csv"
+home <- "House20.csv"
 df <- fread(paste0(rootdir, home))
-df_xts <- xts(df$ChestFreezer, fastPOSIXct(df$Time))
+# home10(ChestFreezer),  home16(Fridge-Freezer_1),home18(Fridge-Freezer),  home20(Freezer)
+df_xts <- xts(df$'Freezer', fastPOSIXct(df$Time))
 df_xts_sub <- resample_UTC_data_minutely(df_xts, 1)
 
-day1 <- df_xts_sub['2014-04-04']
-day2 <- df_xts_sub['2014-04-05']
+date1 <- '2014-05-05' # normal
+date2 <-'2014-06-21'  # anomalous
+day1 <- df_xts_sub[date1]
+day2 <- df_xts_sub[date2]
 temp <- data.frame(coredata(day1), coredata(day2))
 colnames(temp) <-  c('day1','day2')
 temp$timestamp = index(day1)
 df_long <- reshape2::melt(temp,id.vars = "timestamp")
-
 g <- ggplot(df_long,aes(timestamp,value ,group = variable,color = variable, size = variable)) + geom_line()
 g <- g + scale_color_manual(values=c('blue','black')) + scale_size_manual(values=c(0.4, 0.7))
-g <- g + theme(legend.position="none",axis.text=element_text(color="black",size=10)) + scale_x_datetime(labels=scales::date_format("%H:%M")) + labs(x= "Hour of the day", y = "Power (W)")
+g <- g + theme(legend.position="none",axis.text=element_text(color="black")) + scale_x_datetime(labels=scales::date_format("%H:%M")) + labs(x= "Hour of the day", y = "Power (W)")
 g
-#savename <- paste0(inspec_date,"_",strsplit(filename,'[.]')[[1]][1],".pdf")
+savename <- paste0(strsplit(home,'[.]')[[1]][1],'_',date1,'_',date2,".pdf")
+ggsave(savename, width = 3 ,height = 2 ,units=c('in'))
--- R_codes/sensitivity_analysis.R
+++ R_codes/sensitivity_analysis.R
@@ -1,5 +1,6 @@
 # date: 14 march 2018
 # author: haroonr
+library(Cairo)
 library(ggplot2)
 #library(data.table)
 setwd('/Volumes/MacintoshHD2/Users/haroonr/Detailed_datasets/REFITT/Intermediary_results/paper_draft/paper_plots/')
@@ -35,11 +36,14 @@ keep <- c('home','alpha','fscore')
 df_sub <- df_alpha[keep]
 df_long = reshape2::melt(df_sub,id.vars=c('home','alpha'))
 g <- ggplot(data=df_long,aes(alpha,value,group = home,color=home)) + geom_line() + geom_point(aes(shape=home))
-g <- g +  labs(x = "alpha value", y='Fscore')  + scale_color_brewer(palette = 'Set1')
+g <- g +  labs(x = "\u03B1", y='Fscore')  + scale_color_brewer(palette = 'Set1')
 g <- g + theme(axis.text = element_text(color="Black",size = 9),legend.text = element_text(size = 7),legend.position = c(0.5,0.18), legend.title = element_blank())
 g <- g + guides(col = guide_legend(nrow = 2 )) 
 g
 ggsave("alpha_senstivity.pdf", width = 4, height = 2.5,units = "in")
+#cairo_pdf("alpha_temp.pdf", width = 4, height = 2.5)
+
+
 
 
 df_sigma = df[df$context_no == 4 & df$alpha == 2,]
@@ -48,7 +52,7 @@ keep <- c('home','sigma','fscore')
 df_sub <- df_sigma[keep]
 df_long = reshape2::melt(df_sub,id.vars=c('home','sigma'))
 g <- ggplot(data=df_long,aes(sigma,value, group = home, color=home)) + geom_line() + geom_point(aes(shape=home))
-g <- g +  labs(x = "Sigma value", y='Fscore')  + scale_color_brewer(palette = 'Set1')
+g <- g +  labs(x = "n", y='Fscore')  + scale_color_brewer(palette = 'Set1')
 g <- g + theme(axis.text = element_text(color="Black",size = 9),legend.text = element_text(size = 7),legend.position = c(0.6,0.18), legend.title = element_blank())
 g <- g + guides(col = guide_legend(nrow = 2 )) 
 g
